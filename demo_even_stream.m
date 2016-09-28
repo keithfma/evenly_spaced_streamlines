2f8febb86d07a041974b706a8e0c87351c891f08
Keith Ma <keithfma@gmail.com>
2016-09-27 21:30:01 -0400

    Added skeletons for main program and demo script

---
+++ demo_even_stream.m
@@ -0,0 +1,18 @@
+% Run even_stream() for an example vector field and a range of input
+% parameter options
+
+%% Intialize example vector field
+% see documentation for gradient()
+
+vv = linspace(-2, 2, 20);
+hh = vv(2)-vv(1);
+[xx, yy] = meshgrid(vv);
+zz = xx .* exp(-xx.^2 - yy.^2);
+[dzdx, dzdy] = gradient(zz, hh, hh);
+
+%% Plot evenly-spaced streamlines
+
+d_sep = 0.05*range(vv);
+d_test = 0.5*d_sep;
+
+even_stream(xx, yy, dzdx, dzdy, d_sep, d_test);
---
+++ even_stream.m
@@ -0,0 +1,19 @@
+function [] = even_stream(xx, yy, uu, vv, d_sep, d_test) %#ok!
+% function [] = even_stream(xx, yy, uu, vv, d_sep, d_test)
+%
+% Plot evenly-spaced streamlines with Jobar & Lefer algorithm (ref 1).
+%
+% Arguments:
+%   xx, yy:
+%   uu, vv:
+%   d_sep:
+%   d_test:
+% 
+% References: 
+% [1] Jobard, B., & Lefer, W. (1997). Creating Evenly-Spaced Streamlines of
+%   Arbitrary Density. In W. Lefer & M. Grave (Eds.), Visualization in
+%   Scientific Computing ’97: Proceedings of the Eurographics Workshop in
+%   Boulogne-sur-Mer France, April 28--30, 1997 (pp. 43–55). inbook,
+%   Vienna: Springer Vienna. http://doi.org/10.1007/978-3-7091-6876-9_5
+% %
+
