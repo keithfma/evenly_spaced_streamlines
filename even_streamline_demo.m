7817ef81be9f9508a3afef9c5a42eaf3a43fe75e
Keith Ma <keithma@bu.edu>
2016-09-30 13:08:52 -0400

    Minor update to demo

--- even_streamline_demo.m
+++ even_streamline_demo.m
@@ -1,17 +1,10 @@
-%% Intialize example vector field
-% see documentation for gradient()
+%% Compute streamline data for example vector field
 
 vv = linspace(-2, 2, 20);
 hh = vv(2)-vv(1);
 [xx, yy] = meshgrid(vv);
 zz = xx .* exp(-xx.^2 - yy.^2);
-% %<DEBUG> Confirm program can deal with NaNs
-% zz(xx>1.5) = NaN;
-% %</DEBUG>
 [dzdx, dzdy] = gradient(zz, hh, hh);
-
-%% Plot evenly-spaced streamlines
-
 d_sep = 0.05*range(vv);
 d_test = 0.5*d_sep;
 step_size = 0.1;
@@ -20,6 +13,8 @@ verbose = true;
 xy = get_stream_xy(xx, yy, dzdx, dzdy, d_sep, d_test, step_size, verbose);
 len = get_stream_len(xy, verbose);
 dist = get_stream_dist(xy, verbose);
+
+%% Example plots
 
 %<DEBUG>
 hold off
