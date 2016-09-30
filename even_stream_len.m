7ffca1c273e3a8bbe23acd56e2b4f7374575cac0
Keith Ma <keithma@bu.edu>
2016-09-30 11:36:25 -0400

    Added header information for even_stream_len

---
+++ even_stream_len.m
@@ -0,0 +1,22 @@
+function len = even_stream_len(xy)
+%
+% Return arc length of streamlines created with the even_stream_xy fuction.
+% This step is part of the Jobar & Lefer [1] algorithm to plot evenly
+% spaced streamlines with along-line texture. 
+%
+% Arguments:
+%   xy = Matrix, evenly-spaced streamlines, as created by the
+%       even_stream_xy function, with [x,y] points in rows, and lines
+%       separated by NaNs
+%   len = Vector, arc length (distance along the line) for all stream line
+%       points in xy
+%
+% References: 
+% [1] Jobard, B., & Lefer, W. (1997). Creating Evenly-Spaced Streamlines of
+%   Arbitrary Density. In W. Lefer & M. Grave (Eds.), Visualization in
+%   Scientific Computing ?97: Proceedings of the Eurographics Workshop in
+%   Boulogne-sur-Mer France, April 28--30, 1997 (pp. 43?55). inbook,
+%   Vienna: Springer Vienna. http://doi.org/10.1007/978-3-7091-6876-9_5
+% %
+
+error('%s not yet implemented', mfilename);
