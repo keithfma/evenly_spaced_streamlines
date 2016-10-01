609503b42f5ac6db9292694dbc4aa8ebec40d6d1
Keith Ma <keithfma@gmail.com>
2016-09-30 22:51:14 -0400

    Implemented _arrow

--- even_streamline_arrow.m
+++ even_streamline_arrow.m
@@ -2,7 +2,8 @@ function [hl, ha] = even_streamline_arrow(xx, yy, uu, vv, d_sep, d_test, varargi
 % function [hl, ha] = even_streamline_arrow(xx, yy, uu, vv, d_sep, d_test, varargin)
 %
 % Plot evenly-spaced streamlines with Jobar & Lefer algorithm [1] with
-% arrow glyphs to indicate the flow direction.
+% arrow glyphs to indicate the flow direction. Uses the 'arrow' package by
+% Dr. Erik A. Johnson from the Mathworks FileExchange.
 %
 % Arguments:
 %   xx, yy, uu, vv: Vector field x-coord, y-coord, vector x-component and
@@ -16,10 +17,10 @@ function [hl, ha] = even_streamline_arrow(xx, yy, uu, vv, d_sep, d_test, varargi
 %   'LineStyle': line style, as in the plot(), default = '-'
 %   'LineWidth': line width, as in the plot(), default = 0.5
 %   'Color': line and arrow color, as in plot(), default = 'b'
-%   'HeadStyle': arrow head style, as in annotation(), default = 'vback2'
-%   'HeadLength': arrow head length, as in annotation(), default = 10
-%   'HeadWidth': arrow head width, as in annotation(), default = 10
-%   'HeadSpc': arrow head spacing along line in points, default = 10
+%   'ArrowLength': arrow head length in pixels, default = 20
+%   'ArrowTipAngle': arrow head tip angle in degrees, default = 20
+%   'ArrowBaseAngle': arrow head base angle in degrees, default = 10
+%   'ArrowSpace': arrow head spacing along line in # points, default = 10
 %   
 % Returns:
 %   hl = Graphics object for streamlines
@@ -45,50 +46,39 @@ parser.addParameter('verbose', false);
 parser.addParameter('LineStyle', '-');
 parser.addParameter('LineWidth', 0.5);
 parser.addParameter('Color', 'b');
-parser.addParameter('HeadStyle', 'vback2');
-parser.addParameter('HeadLength', 10);
-parser.addParameter('HeadWidth', 10);
-parser.addParameter('HeadSpc', 10);
+parser.addParameter('ArrowLength', 7);
+parser.addParameter('ArrowTipAngle', 20);
+parser.addParameter('ArrowBaseAngle', 45);
+parser.addParameter('ArrowSpacing', 5);
 
 parser.parse(varargin{:});
 step_size = parser.Results.stepsize;
 verbose = parser.Results.verbose;
 line_style = parser.Results.LineStyle;
 line_width = parser.Results.LineWidth;
-line_color = parser.Results.Color;
-head_style = parser.Results.HeadStyle;
-head_length = parser.Results.HeadLength;
-head_width = parser.Results.HeadWidth;
-head_spc= parser.Results.HeadSpc;
+color = parser.Results.Color;
+arrow_length = parser.Results.ArrowLength;
+arrow_tip_angle = parser.Results.ArrowTipAngle;
+arrow_base_angle = parser.Results.ArrowBaseAngle;
+arrow_spacing = parser.Results.ArrowSpacing;
 
 % get streamline data
 xy = get_stream_xy(xx, yy, uu, vv, d_sep, d_test, step_size, verbose);
 
 % plot lines
 hl = plot(xy(:,1), xy(:,2), ...
-    'LineStyle', line_style, 'LineWidth', line_width, 'Color', line_color);
+    'LineStyle', line_style, 'LineWidth', line_width, 'Color', color);
 
 % plot arrows
-xy_from = xy(head_spc:head_spc:end, :);
-xy_to = xy(1+head_spc:head_spc:end, :);
+xy_from = xy(arrow_spacing:arrow_spacing:end, :);
+xy_to = xy(1+arrow_spacing:arrow_spacing:end, :);
 xy_nan = any(isnan(xy_from), 2) | any(isnan(xy_to), 2);
 xy_from = xy_from(~xy_nan, :);
 xy_to = xy_to(~xy_nan, :);
+axis(axis); % recommended in arrow() to fix axes limits 
 
-arrow(xy_from, xy_to);
-keyboard
-
-% % plot arrows 
-% % NOTE: uses the annotation() function to get decent arrowhead markers, but
-% % this plots in normalized coordinates.
-% x_arrow = [xy(head_spc:head_spc:end, 1), xy(1+head_spc:head_spc:end, 1)];
-% y_arrow = [xy(head_spc:head_spc:end, 2), xy(1+head_spc:head_spc:end, 2)];
-% nan_arrow = any(isnan(x_arrow), 2) | any(isnan(y_arrow), 2);
-% x_arrow = x_arrow(~nan_arrow, :);
-% y_arrow = y_arrow(~nan_arrow, :);
-% keyboard
-% ax = gca;
-% norm_x_arrow = (x_arrow-ax.XLim(1))/(ax.XLim(2)-ax.XLim(1));
-% norm_y_arrow = (y_arrow-ax.YLim(1))/(ax.YLim(2)-ax.YLim(1));
-% ha = [];
-
+ha = arrow(xy_from, xy_to, ...
+    'Length', arrow_length, ...
+    'TipAngle', arrow_tip_angle, ...
+    'BaseAngle', arrow_base_angle, ...
+    'FaceColor', color, 'LineStyle', 'none');
