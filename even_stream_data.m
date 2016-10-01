8137205accbd3bcbf84593d19309566ac6261296
Keith Ma <keithfma@gmail.com>
2016-10-01 11:57:20 -0400

    Started work on single function to compute streamline data

---
+++ even_stream_data.m
@@ -0,0 +1,248 @@
+function xyld = even_stream_data(xx, yy, uu, vv, varargin) 
+% function xyld = even_stream_data(xx, yy, uu, vv, varargin) 
+%
+% Compute evenly-spaced streamlines with Jobar & Lefer algorithm [1].
+% Always returns streamline points. Optionally, return arc length
+% (distance along each streamline) and minimum distance to neighboring
+% lines.
+%
+% Arguments:
+%   xx, yy: Vector field x-coord, y-coord
+%   uu, vv: Vector field x-component and y-component
+%
+% Optional Parameters (Name - Value):
+%   'DistSep': Scalar, minimum distance between seed point and streamlines, 
+%       default = 5% minimum domain width 
+%   'DistTest': Scalar, minimum distance between streamlines, 
+%       default = 2% minimum domain width 
+%   'StepSize': Scalar, streamline step size, see streamline() for details,
+%       default = 0.1
+%   'GetLength': Set true to compute and return 'len',
+%       default = false
+%   'GetDist': Set true to compute and return 'dist',
+%       default = false
+%   'Verbose': set true to enable verbose messages, 
+%       default = false
+%
+% Return: 
+%   xyld: Matrix, [x, y, len, dist] for stream line points, each row is a point, 
+%       individual lines are separated by NaNs, x is the x-coord, y is the
+%       y-coord, len is the arc length (distance along the line), dist is
+%       the minimum distance to neighboring stream lines. 'len' and 'dist'
+%       are computed only if 'GetLen' and 'GetDist' are set to true,
+%       respectively, otherwise, these columns are set to NaN.
+%  
+% References: 
+% [1] Jobard, B., & Lefer, W. (1997). Creating Evenly-Spaced Streamlines of
+%   Arbitrary Density. In W. Lefer & M. Grave (Eds.), Visualization in
+%   Scientific Computing ?97: Proceedings of the Eurographics Workshop in
+%   Boulogne-sur-Mer France, April 28--30, 1997 (pp. 43?55). inbook,
+%   Vienna: Springer Vienna. http://doi.org/10.1007/978-3-7091-6876-9_5
+% %
+
+% parse and check inputs
+parser = inputParser;
+parser.CaseSensitive = false;
+parser.PartialMatching = false;
+parser.KeepUnmatched = false;
+
+parser.addParameter('DistSep', 0.05*min(range(xx(:)), range(yy(:))));
+parser.addParameter('DistTest', 0.02*min(range(xx(:)), range(yy(:))) );
+parser.addParameter('StepSize', 0.1);
+parser.addParameter('GetLength', false);
+parser.addParameter('GetDist', false);
+parser.addParameter('Verbose', false);
+
+parser.parse(varargin{:});
+dist_sep = parser.Results.DistSep;
+dist_test = parser.Results.DistTest;
+step_size = parser.Results.StepSize;
+get_len = parser.Results.GetLength;
+get_dist = parser.Results.GetDist;
+verbose = parser.Results.Verbose;
+
+sanity_check(xx, yy, uu, vv, dist_sep, dist_test, step_size, get_len, ...
+    get_dist, verbose);
+
+keyboard
+
+% disable nuisance warning(s)
+% ...Delaunay triangulation drops duplicate points from streamlines, OK
+warning('off', 'MATLAB:delaunayTriangulation:DupPtsWarnId'); 
+
+% select random non-NaN grid point for initial seed
+while 1
+  kk = randi([1, numel(xx)]);
+  if ~isnan(uu(kk)) && ~isnan(vv(kk))
+      break
+  end
+end
+seed_xy = [xx(kk), yy(kk)];
+
+% add first streamline to triangulation and length list
+[stream_xy, ~] = get_streamline(xx, yy, uu, vv, seed_xy, step_size);
+stream_tri = delaunayTriangulation(stream_xy);
+stream_len = size(stream_tri.Points,1);
+
+% create seed point candidate queue 
+seed_queue{1} = get_seed_candidates(stream_xy, d_sep);
+
+% check all seed candidates
+while ~isempty(seed_queue)
+    
+    % pop seed candidates from queue
+    seed_xy = seed_queue{1}; 
+    seed_queue(1) = []; 
+    
+    % check each seed candidate in random order
+    for ii = randperm(length(seed_xy))
+        
+        % skip if candidate istoo close to any streamline point
+        [~, d_min] = nearestNeighbor(stream_tri, seed_xy(ii,:));
+        if d_min < d_sep
+            continue
+        end
+                
+        % create new streamline, skip if empty
+        [stream_xy, seed_idx] = get_streamline(xx, yy, uu, vv, seed_xy(ii,:), step_size);
+        if size(stream_xy,1) < 2
+            continue
+        end 
+         
+        % trim new streamline        
+        for jj = seed_idx:size(stream_xy,1)
+            [~, d_min] = nearestNeighbor(stream_tri, stream_xy(jj,:));
+            if d_min < d_test
+                break
+            end
+        end
+        for kk = seed_idx:-1:1
+            [~, d_min] = nearestNeighbor(stream_tri, stream_xy(kk,:));
+            if d_min < d_test
+                break
+            end
+        end
+        stream_xy = stream_xy(kk:jj, :);
+        
+        % add streamline to triangulation and line length list
+        len0 = size(stream_tri.Points, 1);
+        stream_tri.Points = [stream_tri.Points; stream_xy];
+        stream_len(end+1) = size(stream_tri.Points, 1)-len0; %#ok!
+         
+        % add seed candidate points to queue
+        seed_queue{end+1}  = get_seed_candidates(stream_xy, d_sep); %#ok!
+        
+        if verbose
+            num_lines = length(stream_len);
+            num_seeds = 0;
+            for pp = 1:length(seed_queue)
+                num_seeds = num_seeds+length(seed_queue{pp});
+            end
+            fprintf('%s: %d lines, %d seed candidates\n', ...
+                mfilename, num_lines, num_seeds);
+        end
+
+    end
+end
+
+% extract stream line points for output as xy 
+num_pts = size(stream_tri.Points, 1);
+num_lines = length(stream_len);
+xy = nan(num_pts+num_lines-1, 2);
+ii0 = 1; 
+jj0 = 1;
+for kk = 1:num_lines
+    ii1 = ii0+stream_len(kk)-1;
+    jj1 = jj0+stream_len(kk)-1;    
+    xy(jj0:jj1,:) = stream_tri.Points(ii0:ii1,:);    
+    ii0 = ii1+1;
+    jj0 = jj1+2;
+end
+
+% enable nuisance warning(s)
+warning('on', 'MATLAB:delaunayTriangulation:DupPtsWarnId'); 
+
+function [] = sanity_check(xx, yy, uu, vv, dist_sep, dist_test, ...
+    step_size, get_len, get_dist, verbose)
+% function [] = sanity_check(xx, yy, uu, vv, dist_sep, dist_test, ...
+%     step_size, get_len, get_dist, verbose)
+% 
+% Check for valid inputs, fail with error if any are invalid
+% %
+
+validateattributes(xx, {'numeric'}, {'nonempty'}, ...
+    mfilename, 'xx');
+validateattributes(yy, {'numeric'}, {'size', size(xx)}, ...
+    mfilename, 'yy');
+validateattributes(uu, {'numeric'}, {'size', size(xx)}, ...
+    mfilename, 'uu');
+validateattributes(vv, {'numeric'}, {'size', size(xx)}, ...
+    mfilename, 'vv');
+validateattributes(dist_sep, {'numeric'}, {'scalar', 'positive'}, ...
+    mfilename, 'dist_sep');
+validateattributes(dist_test, {'numeric'}, {'scalar', 'positive', '<=', dist_sep}, ...
+    mfilename, 'dist_test');
+validateattributes(step_size, {'numeric'}, {'scalar', 'positive'}, ...
+    mfilename, 'step_size');
+validateattributes(get_len, {'numeric', 'logical'}, {'scalar', 'binary'}, ...
+    mfilename, 'get_len');
+validateattributes(get_dist, {'numeric', 'logical'}, {'scalar', 'binary'}, ...
+    mfilename, 'get_dist');
+validateattributes(verbose, {'numeric', 'logical'}, {'scalar', 'binary'}, ...
+    mfilename, 'verbose');
+
+function seed_xy = get_seed_candidates(xy, buf_dist)
+%
+% Compute the location of stream line seed point candidates that lie at a
+% distance 'buf_dist' along a normal vector at each point in 'xy' 
+%
+% Arguments:
+%   xy: Matrix, [x,y] coordinates of points along a streamline in rows
+%   buf_dist: Scalar, buffer distance between xy and seed point candidates
+%   seed_xy, y_seed: Vectors, canditate seed points
+% %
+
+% get unit normal vectors at segment midpoints
+tangent = diff(xy);
+normal = [tangent(:,2), -tangent(:,1)];
+normal = bsxfun(@rdivide, normal, sqrt(sum(normal.*normal, 2)));
+midpoint = xy(1:end-1, :)+0.5*tangent;
+
+% get candidates offset buf_dist in positive and negative normal direction
+seed_xy = [midpoint + buf_dist*normal; midpoint - buf_dist*normal];
+
+
+function [stream_xy, seed_idx] = get_streamline(xx, yy, uu, vv, seed_xy, step_size)
+%
+% Compute streamline in both directions starting at seed point
+%
+% Arguments: 
+%   xx, yy:
+%   uu, vv:
+%   seed_xy: Vector, [x, y] coordinates of seed point
+%   stream_xy : Matrix, stream line x- and y-coordinates in rows, returns [] if
+%       stream line has zero length
+%   seed_idx: Scalar, index of seed point in output streamline
+% %
+
+fwd = stream2(xx, yy, uu, vv, seed_xy(1), seed_xy(2), step_size);
+xy_fwd = fwd{1}(~any(isnan(fwd{1}), 2), :); % drop NaN rows
+has_fwd = size(xy_fwd,1) > 1;
+
+rev = stream2(xx, yy, -uu, -vv, seed_xy(1), seed_xy(2), step_size);
+xy_rev = rev{1}(~any(isnan(rev{1}), 2), :); % drop NaN rows
+has_rev = size(xy_rev,1) > 1;
+
+if has_fwd && has_rev
+    stream_xy = [xy_rev(end:-1:2, :); xy_fwd];
+    seed_idx = size(xy_rev,1);
+elseif has_rev
+    stream_xy = xy_rev;
+    seed_idx = 1;
+elseif has_fwd
+    stream_xy = xy_fwd;
+    seed_idx = 1;
+else
+    stream_xy = [];
+    seed_idx = [];
+end
--- even_stream_demo.m
+++ even_stream_demo.m
@@ -5,9 +5,7 @@ hh = vv(2)-vv(1);
 [xx, yy] = meshgrid(vv);
 zz = xx .* exp(-xx.^2 - yy.^2);
 [dzdx, dzdy] = gradient(zz, hh, hh);
-d_sep = 0.05*range(vv);
-d_test = 0.5*d_sep;
-step_size = 0.1;
+
 
 % %<DEBUG> Compute streamline data
 % verbose = true;
@@ -16,19 +14,29 @@ step_size = 0.1;
 % dist = get_stream_dist(xy, verbose);
 % %</DEBUG>
 
-%% Example plots
+%% Compute streamline data
 
-hold off
+d_sep = 0.05*range(vv);
+d_test = 0.5*d_sep;
+step_size = 0.1;
 
-% hh = even_stream_demo(xx, yy, dzdx, dzdy, d_sep, d_test, ...
-%     'Color', 'r', 'LineWidth', 2, 'Verbose', 1);
+xyld = even_stream_data(xx, yy, dzdx, dzdy, ...
+    'DistSep', d_sep, 'DistTest', d_test, 'StepSize', step_size, ...
+    'GetLength', true, 'GetDist', true, 'Verbose', true);
 
-hh = even_stream_taper(xx, yy, dzdx, dzdy, d_sep, d_test, ...
-    'Color', 'r', 'LineWidthMin', 0.5, 'LineWidthMax', 5, 'Verbose', 1);
+%% Plot lines in all styles
 
-% [hl, ha] = even_stream_arrow(xx, yy, dzdx, dzdy, d_sep, d_test, ...
-%     'Color', 'r', 'LineWidth', 1, 'Verbose', 1);
-
-% hh = even_stream_texture(xx, yy, dzdx, dzdy, d_sep, d_test, ...
-%     'Verbose', 1, 'LineWidth', 3);
+% hold off
+% 
+% % hh = even_stream_demo(xx, yy, dzdx, dzdy, d_sep, d_test, ...
+% %     'Color', 'r', 'LineWidth', 2, 'Verbose', 1);
+% 
+% hh = even_stream_taper(xx, yy, dzdx, dzdy, d_sep, d_test, ...
+%     'Color', 'r', 'LineWidthMin', 0.5, 'LineWidthMax', 5, 'Verbose', 1);
+% 
+% % [hl, ha] = even_stream_arrow(xx, yy, dzdx, dzdy, d_sep, d_test, ...
+% %     'Color', 'r', 'LineWidth', 1, 'Verbose', 1);
+% 
+% % hh = even_stream_texture(xx, yy, dzdx, dzdy, d_sep, d_test, ...
+% %     'Verbose', 1, 'LineWidth', 3);
 
