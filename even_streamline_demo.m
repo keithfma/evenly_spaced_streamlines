6afd032785793f376518fdd6ee443e20fea59edd
Keith Ma <keithfma@gmail.com>
2016-10-01 11:17:02 -0400

    Much faster plotting in _taper

--- even_streamline_demo.m
+++ even_streamline_demo.m
@@ -5,7 +5,7 @@ hh = vv(2)-vv(1);
 [xx, yy] = meshgrid(vv);
 zz = xx .* exp(-xx.^2 - yy.^2);
 [dzdx, dzdy] = gradient(zz, hh, hh);
-d_sep = 0.01*range(vv);
+d_sep = 0.05*range(vv);
 d_test = 0.5*d_sep;
 step_size = 0.1;
 
@@ -23,12 +23,12 @@ hold off
 % hh = even_streamline(xx, yy, dzdx, dzdy, d_sep, d_test, ...
 %     'Color', 'r', 'LineWidth', 2, 'Verbose', 1);
 
-% hh = even_streamline_taper(xx, yy, dzdx, dzdy, d_sep, d_test, ...
-%     'Color', 'r', 'LineWidthMin', 0.5, 'LineWidthMax', 5, 'Verbose', 1);
+hh = even_streamline_taper(xx, yy, dzdx, dzdy, d_sep, d_test, ...
+    'Color', 'r', 'LineWidthMin', 0.5, 'LineWidthMax', 5, 'Verbose', 1);
 
 % [hl, ha] = even_streamline_arrow(xx, yy, dzdx, dzdy, d_sep, d_test, ...
 %     'Color', 'r', 'LineWidth', 1, 'Verbose', 1);
 
-hh = even_streamline_texture(xx, yy, dzdx, dzdy, d_sep, d_test, ...
-    'Verbose', 1, 'LineWidth', 3);
+% hh = even_streamline_texture(xx, yy, dzdx, dzdy, d_sep, d_test, ...
+%     'Verbose', 1, 'LineWidth', 3);
 
--- even_streamline_taper.m
+++ even_streamline_taper.m
@@ -55,26 +55,45 @@ line_color = parser.Results.Color;
 xy = get_stream_xy(xx, yy, uu, vv, d_sep, d_test, step_size, verbose);
 dist = get_stream_dist(xy, verbose);
 
-% create plot
+% reformat streamlines as segments 
 num_segments = size(xy,1)-2*sum(isnan(xy(:,1)))-1;
-hh = gobjects(num_segments,1);
+x_segment = nan(num_segments, 2);
+y_segment = nan(num_segments, 2);
+d_segment = nan(num_segments, 1);
 current_segment = 0;
-hold on;
 for ii = 1:length(dist)-1           
     % skip line endpoints, nothing to plot
     if any(isnan(xy(ii,:))) || any(isnan(xy(ii+1,:)))
         continue
     end
-    % count and report
     current_segment = current_segment+1;
-    if verbose 
-        fprintf('%s: segment %d of %d\n', mfilename, current_segment, num_segments);
-    end 
-    % compute segment width
-    d_seg = 0.5*(dist(ii)+dist(ii+1));
-    w_coef_seg = max(0.001, min(1, (d_seg-d_test)/(d_sep-d_test)));
-    w_seg = line_width_min+w_coef_seg*(line_width_max-line_width_min);
-    % plot segment
-    hh(current_segment) = plot(xy(ii:ii+1,1), xy(ii:ii+1,2), ...
-        'LineStyle', line_style, 'LineWidth', w_seg, 'Color', line_color);    
+    x_segment(current_segment, :) = xy(ii:ii+1,1);
+    y_segment(current_segment, :) = xy(ii:ii+1,2);
+    d_segment(current_segment) = 0.5*(dist(ii)+dist(ii+1));
+end
+
+% get segment width rounded to 0.1 pt
+w_coef = max(0.001, min(1, (d_segment-d_test)/(d_sep-d_test)));
+w_segment = line_width_min+w_coef*(line_width_max-line_width_min);
+w_segment = round(10*w_segment)/10;
+
+% create plot: for efficieny, plot all segments with the same width at once
+unique_widths = unique(w_segment);
+num_unique_widths = length(unique_widths); 
+hh = gobjects(num_unique_widths,1);
+for ii = 1:num_unique_widths
+    % get segments of this width
+    ww = unique_widths(ii);
+    is_ww = find(w_segment==ww);
+    num_segment_ww = length(is_ww);
+    x_segment_ww = nan(3*num_segment_ww, 1);
+    x_segment_ww(1:3:end) = x_segment(is_ww, 1);
+    x_segment_ww(2:3:end) = x_segment(is_ww, 2);    
+    y_segment_ww = nan(3*num_segment_ww, 1);
+    y_segment_ww(1:3:end) = y_segment(is_ww, 1);
+    y_segment_ww(2:3:end) = y_segment(is_ww, 2);    
+    % plot all segments at once
+    hh(ii) = plot(x_segment_ww, y_segment_ww, ...
+        'LineStyle', line_style, 'LineWidth', ww, 'Color', line_color);   
+    hold on
 end
