1848f772223a10b6a56b46fe5d290c593d8331e6
Keith Ma <keithfma@gmail.com>
2016-09-30 22:10:35 -0400

    Using arrow package from FEX to handle _arrow plot

--- even_streamline_arrow.m
+++ even_streamline_arrow.m
@@ -68,12 +68,27 @@ xy = get_stream_xy(xx, yy, uu, vv, d_sep, d_test, step_size, verbose);
 hl = plot(xy(:,1), xy(:,2), ...
     'LineStyle', line_style, 'LineWidth', line_width, 'Color', line_color);
 
-% TODO: plot arrows
-x_arrow = [xy(head_spc:head_spc:end, 1), xy(1+head_spc:head_spc:end, 1)];
-y_arrow = [xy(head_spc:head_spc:end, 2), xy(1+head_spc:head_spc:end, 2)];
-nan_arrow = any(isnan(x_arrow), 2) | any(isnan(y_arrow), 2);
-x_arrow = x_arrow(~nan_arrow, :);
-y_arrow = y_arrow(~nan_arrow, :);
+% plot arrows
+xy_from = xy(head_spc:head_spc:end, :);
+xy_to = xy(1+head_spc:head_spc:end, :);
+xy_nan = any(isnan(xy_from), 2) | any(isnan(xy_to), 2);
+xy_from = xy_from(~xy_nan, :);
+xy_to = xy_to(~xy_nan, :);
+
+arrow(xy_from, xy_to);
 keyboard
-ha = [];
+
+% % plot arrows 
+% % NOTE: uses the annotation() function to get decent arrowhead markers, but
+% % this plots in normalized coordinates.
+% x_arrow = [xy(head_spc:head_spc:end, 1), xy(1+head_spc:head_spc:end, 1)];
+% y_arrow = [xy(head_spc:head_spc:end, 2), xy(1+head_spc:head_spc:end, 2)];
+% nan_arrow = any(isnan(x_arrow), 2) | any(isnan(y_arrow), 2);
+% x_arrow = x_arrow(~nan_arrow, :);
+% y_arrow = y_arrow(~nan_arrow, :);
+% keyboard
+% ax = gca;
+% norm_x_arrow = (x_arrow-ax.XLim(1))/(ax.XLim(2)-ax.XLim(1));
+% norm_y_arrow = (y_arrow-ax.YLim(1))/(ax.YLim(2)-ax.YLim(1));
+% ha = [];
 
