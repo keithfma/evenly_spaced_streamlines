f68f7880df8f4c622c9200cdec080e49fffb9dd7
Keith Ma <keithfma@gmail.com>
2016-10-01 22:34:05 -0400

    Updated _texture to use _data

--- even_stream_demo.m
+++ even_stream_demo.m
@@ -6,22 +6,23 @@ hh = vv(2)-vv(1);
 zz = xx .* exp(-xx.^2 - yy.^2);
 [dzdx, dzdy] = gradient(zz, hh, hh);
 
-%% Compute streamline data
-
-dist_sep = 0.05*range(vv);
-dist_test = 0.5*dist_sep;
-step_size = 0.1;
-
-xyld = even_stream_data(xx, yy, dzdx, dzdy, ...
-    'DistSep', dist_sep, 'DistTest', dist_test, 'StepSize', step_size, ...
-    'GetLength', true, 'GetDist', true, 'Verbose', true);
-
 %% Plot lines in all styles
 
+% streamline parameters
+low_dist_sep = 0.05*range(vv);
+low_dist_test = 0.5*low_dist_sep;
+high_dist_sep = 0.003*range(vv);
+high_dist_test = 0.5*high_dist_sep;
+step_size = 0.1;
+
+% create figure
 hf = figure;
 hf.Name = mfilename;
 
 % normal lines
+xyld = even_stream_data(xx, yy, dzdx, dzdy, ...
+    'DistSep', low_dist_sep, 'DistTest', low_dist_test);
+
 ax = subplot(2,2,1);
 even_stream_line(xyld, 'LineWidth', 0.5, 'Color', 'k', 'LineStyle', '-');
 title('even\_stream\_line');
@@ -29,6 +30,9 @@ ax.XTick = [];
 ax.YTick = [];
 
 % taper
+xyld = even_stream_data(xx, yy, dzdx, dzdy, ...
+    'DistSep', low_dist_sep, 'DistTest', low_dist_test, 'GetDist', true);
+
 ax = subplot(2,2,2);
 even_stream_taper(xyld, 'LineWidthMin', 0.5, 'LineWidthMax', 2, 'Color', 'k');
 title('even\_stream\_taper');
@@ -36,6 +40,9 @@ ax.XTick = [];
 ax.YTick = [];
 
 % arrow
+xyld = even_stream_data(xx, yy, dzdx, dzdy, ...
+    'DistSep', low_dist_sep, 'DistTest', low_dist_test);
+
 ax = subplot(2,2,3);
 even_stream_arrow(xyld, 'LineStyle', '-', 'LineWidth', 0.5, 'Color', 'k', ...
     'ArrowLength', 2, 'ArrowTipAngle', 30, 'ArrowBaseAngle', 10, ...
@@ -45,7 +52,11 @@ ax.XTick = [];
 ax.YTick = [];
 
 % texture
+xyld = even_stream_data(xx, yy, dzdx, dzdy, ...
+    'DistSep', high_dist_sep, 'DistTest', high_dist_test, 'GetLength', true);
+
 ax = subplot(2,2,4);
+even_stream_texture(xyld, 'LineWidth', 1, 'Period', 40);
 title('even\_stream\_texture');
 ax.XTick = [];
 ax.YTick = [];
--- even_stream_texture.m
+++ even_stream_texture.m
@@ -1,19 +1,15 @@
-function hh = even_streamline_texture(xx, yy, uu, vv, d_sep, d_test, varargin)
-% function hh = even_streamline_texture(xx, yy, uu, vv, d_sep, d_test, varargin)
+function hh = even_stream_texture(xyld, varargin)
+% function hh = even_stream_texture(xyld, varargin)
 %
 % Plot evenly-spaced streamlines with Jobar & Lefer algorithm [1] using the
 % texturing effect to produce results similar to the line integral
 % convolution (LIC) technique.
 %
 % Arguments:
-%   xx, yy, uu, vv: Vector field x-coord, y-coord, vector x-component and
-%       vector y-component, respectively, sizes must match
-%   d_sep: Scalar, minimum distance between seed points and stream lines
-%   d_test: Scalar, minimum distance between stream lines
+%   xyld: Matrix with columns [x, y, len, dist], as produced by
+%       even_stream_data. Only the x , y, and len columns are needed.
 %
 % Optional Parameters (Name - Value):
-%   'StepSize': stream line step size as in the built-in stream2, default = 0.1
-%   'Verbose': set true to enable verbose messages, default = false
 %   'LineWidth': line width, as in the plot(), default = 0.5
 %   'Period': length of periodic pattern in # points, default = 20;
 %   
@@ -29,43 +25,34 @@ function hh = even_streamline_texture(xx, yy, uu, vv, d_sep, d_test, varargin)
 % %
 
 % handle inputs
-% NOTE: sanity checks are defered to child functions
 parser = inputParser;
 parser.CaseSensitive = false;
 parser.PartialMatching = false;
 parser.KeepUnmatched = false;
 
-parser.addParameter('StepSize', 0.1);
-parser.addParameter('Verbose', false);
 parser.addParameter('LineWidth', 0.5);
 parser.addParameter('Period', 20);
 
 parser.parse(varargin{:});
-step_size = parser.Results.StepSize;
-verbose = parser.Results.Verbose;
 line_width = parser.Results.LineWidth;
 period = parser.Results.Period;
 
 validateattributes(period, {'numeric'}, {'scalar', 'positive', 'integer'}, ...
     mfilename, 'period');
 
-% get streamline data
-xy = get_stream_xy(xx, yy, uu, vv, d_sep, d_test, step_size, verbose);
-len = get_stream_len(xy, verbose);
-
 % reformat streamlines as segments 
-num_segments = size(xy,1)-2*sum(isnan(xy(:,1)))-1;
+num_segments = size(xyld, 1)-2*sum(isnan(xyld(:,1)))-1;
 x_segment = nan(num_segments, 2);
 y_segment = nan(num_segments, 2);
 current_segment = 0;
-for ii = 1:length(len)-1           
+for ii = 1:size(xyld,1)-1           
     % skip line endpoints, nothing to plot
-    if any(isnan(xy(ii,:))) || any(isnan(xy(ii+1,:)))
+    if any(isnan(xyld(ii,1:2))) || any(isnan(xyld(ii+1,1:2)))
         continue
     end
     current_segment = current_segment+1;
-    x_segment(current_segment, :) = xy(ii:ii+1,1);
-    y_segment(current_segment, :) = xy(ii:ii+1,2);
+    x_segment(current_segment, :) = xyld(ii:ii+1,1);
+    y_segment(current_segment, :) = xyld(ii:ii+1,2);
 end
 
 % get colormap
@@ -77,6 +64,8 @@ idx = (1:num_segments)';
 color_coeff = 0.5*(1+sin(2*pi*idx/period)) + mod(idx, period)/(period-1);
 color_coeff = (color_coeff-min(color_coeff))/range(color_coeff);
 c_segment = min(num_colors, max(1, round(num_colors*color_coeff)));
+
+keyboard
 
 % create plot: for efficieny, plot all segments with the same color at once
 unique_colors = unique(c_segment);
