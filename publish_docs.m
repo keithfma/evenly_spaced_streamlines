29fd2e90fb9f58e131e44c97d8f2f79211b70e8e
Keith Ma <keithfma@gmail.com>
2016-10-03 13:08:44 -0400

    Docs to html (for Mathworks) and redirect from docs (for Github)

--- publish_docs.m
+++ publish_docs.m
@@ -2,4 +2,3 @@
 % locations and filenames are chosed to play well with Github Pages.
 
 publish('even_stream_demo.m', 'format', 'html', 'outputDir', 'docs');
-movefile('docs/even_stream_demo.html', 'docs/index.html');
